cf035a49f9eb8b1b19f9ea08e9566f8a72261e8d
PetrKharitonov <70897252+PetrKharitonov@users.noreply.github.com>
2022-06-15 23:55:13 +0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,344 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import * as CANNON from 'cannon-es'
+import { TextGeometry } from 'three'
+
+
+const fontLoader = new THREE.FontLoader()
+
+
+
+
+
+const hitSound = new Audio('/sounds/hit.mp3')
+
+const playHitSound = (collision) => {
+    const impactStrength = collision.contact.getImpactVelocityAlongNormal()
+
+    if (impactStrength > 1.5){
+        hitSound.volume = Math.random() 
+        hitSound.currentTime = 0
+        hitSound.play()
+    }
+}
+
+/**
+ * Debug
+ */
+/* const gui = new dat.GUI()
+const debugObject = {}
+debugObject.createText = () => {
+    createText('олень')
+}
+
+
+debugObject.reset = () => {
+    for (const object of objectsToUpdate) {
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+
+        scene.remove(object.mesh)
+    }
+}
+
+gui.add(debugObject, 'createText')
+gui.add(debugObject, 'reset') */
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+scene.background = new THREE.Color( 0xffffff)
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.png',
+    '/textures/environmentMaps/0/nx.png',
+    '/textures/environmentMaps/0/py.png',
+    '/textures/environmentMaps/0/ny.png',
+    '/textures/environmentMaps/0/pz.png',
+    '/textures/environmentMaps/0/nz.png'
+])
+
+
+// Physics
+
+// World
+const world = new CANNON.World()
+world.broadphase = new CANNON.SAPBroadphase(world)
+world.allowSleep = true
+world.gravity.set(0, - 9.82, 0)
+
+// materials
+
+const defaultMaterial = new CANNON.Material('default')
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
+    {
+        friction: 0.4,
+        restitution: 0.2
+    }
+)
+world.addContactMaterial(defaultContactMaterial)
+
+world.defaultContactMaterial = defaultContactMaterial
+
+
+//Floor
+
+const floorShape = new CANNON.Plane()
+const floorBody = new CANNON.Body()
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI / 2)
+world.addBody(floorBody)
+
+
+
+
+/**
+ * Floor
+ */
+
+const floor = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#777777',
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+//scene.add(floor)
+
+/**  
+ *  Walls
+ **/
+
+ const wall1Shape = new CANNON.Plane()
+ const wall1Body = new CANNON.Body()
+ wall1Body.mass = 0;
+ wall1Body.addShape(wall1Shape);
+ wall1Body.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI)
+ wall1Body.position.z = 0.4
+ world.addBody(wall1Body)
+
+ const wall2Shape = new CANNON.Plane()
+ const wall2Body = new CANNON.Body()
+ wall2Body.mass = 0;
+ wall2Body.addShape(wall2Shape);
+ //wall2Body.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI)
+ wall2Body.position.z = - 0.4
+ world.addBody(wall2Body) 
+ 
+ 
+
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0, 3, 5)
+scene.add(camera)
+
+
+
+// Controls
+/* const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true */
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+/* 
+ // Utils
+ */
+
+const objectsToUpdate = []
+
+// text creator
+
+let randomColor = Math.floor(Math.random()*16777215).toString(16)
+
+
+const createText = (char) => {
+    fontLoader.load(
+        'fonts/Roboto_Bold.json',
+        (font) => {
+            const textGeometry = new THREE.TextBufferGeometry(
+                char,
+                {
+                    font: font,
+                    size: 0.5,
+                    height: 0.1,
+                    curveSegments: 12,
+                    bevelEnabled: true,
+                    bevelThickness: 0.03,
+                    bevelSize: 0.02,
+                    bevelOffset: 0,
+                    bevelSegments: 5
+                }
+            )
+            textGeometry.computeBoundingBox()
+            textGeometry.translate(
+                - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+                - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+                - (textGeometry.boundingBox.max.z - 0.03) * 0.5,
+            )
+            
+            const boxSize = textGeometry.boundingBox.getSize()
+
+            
+            const textMaterial = new THREE.MeshStandardMaterial({
+                color: ['#2E1CEB', '#EBCC1C', '#EB3A28', '#4BEB95'][Math.floor(Math.random() * 4)],
+                metalness: 0.3,
+                roughness: 0.4,
+                envMap: environmentMapTexture
+            })
+
+
+            const text = new THREE.Mesh(textGeometry, textMaterial)
+            scene.add(text)
+
+            const shape = new CANNON.Box(new CANNON.Vec3(boxSize.x * 0.5, boxSize.y * 0.5, boxSize.z * 0.5))
+            const body = new CANNON.Body({
+                mass: boxSize.x,
+                position: new CANNON.Vec3((Math.random()-0.5) * 5, 10, 0),
+                shape,
+                material: defaultMaterial
+            })
+            body.addEventListener('collide', playHitSound)
+            world.addBody(body)
+        
+            
+            objectsToUpdate.push({
+                mesh: text,
+                body: body
+            })
+            
+        }
+    )
+}
+
+
+
+// label
+
+document.getElementById('myButton').onclick = function() {
+
+    let input = document.getElementById('myText').value;
+    document.getElementById('myText').value = ''
+
+    //scene.background = new THREE.Color(['#2E1CEB', '#EBCC1C', '#EB3A28', '#4BEB95'][Math.floor(Math.random() * 4)])
+
+    createText(input)
+
+}
+
+document.querySelector('.reset').onclick = function() {
+    for (const object of objectsToUpdate) {
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+    
+        scene.remove(object.mesh)
+    }
+}
+
+
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let oldElapsedTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
+
+    //Update physics world
+    world.step(1/60, deltaTime, 3)
+
+    for (const object of objectsToUpdate) {
+        object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
+    }
+
+    // Update controls
+    //controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
